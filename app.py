c8ce1470923c3d0ddc682c533cdba416ac81a2db
Aman Satyawali <amansatyawali@gmail.com>
2021-09-05 15:16:59 +0530

    added multiple pages and used the trained model to make a prediction and input/output on the webpage

--- app.py
+++ app.py
@@ -1,5 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.globals import request
+import joblib
+
+model = joblib.load('model/diab_79.pkl')
 
 app = Flask(__name__)
 
-app.run()
+@app.route('/')
+def home_page() : 
+  return render_template('home.html')
+
+@app.route('/userSaved')
+def add_user_data() : 
+  return render_template('userAdded.html')
+
+@app.route('/input')
+def input_patient_data() : 
+  return render_template('input.html')
+
+@app.route('/predict', methods = ['post'])
+def preditct() : 
+  preg = request.form.get('preg')
+  plas = request.form.get('plas')
+  pres = request.form.get('pres')
+  skin = request.form.get('skin')
+  test = request.form.get('test')
+  mass = request.form.get('mass')
+  pedi = request.form.get('pedi')
+  age = request.form.get('age')
+
+  prediction = model.predict([[preg, plas, pres, skin, test, mass, pedi, age]])
+  if prediction == 1 :
+    prediction = 'Diabetic'
+  else : 
+    prediction = 'Non diabetic'
+  return render_template('prediction.html', prediction = prediction)
+  
+app.run(debug = True)
